12674ae59439b908e0730fc07ed44a62d47ceee8
OliverStat <oliverstatt@web.de>
2017-08-01 13:31:06 +0200

    added CORS, heroku seem to have changed something that made this necessary

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load required modules
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes/api');
 
 const app = express();
@@ -13,34 +14,38 @@ mongoose.connect('mongodb://heroku_jtj9mgwt:rcvs815upbfcvujccf04pahgah@ds161022.
 const db = mongoose.connection;
 
 db.on('error', err => {
-  console.log("connection error: " + err);
+    console.log("connection error: " + err);
 });
 db.once("open", () => {
-  console.log("connected to mongoDB...");
+    console.log("connected to mongoDB...");
 });
 
-// Middleware | TO DO: clear important params from req object
+// Middleware
 // serves static page / frontend
 app.use(express.static(__dirname + '/public'));
+
+// Access Control Allow Origin
+
+app.use(cors());
 
 // setting up the router
 app.use('/api', routes);
 
 // catch all 404 errors and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // Error handler
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message
-    }
-  });
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
 });
 
 module.exports = app;
